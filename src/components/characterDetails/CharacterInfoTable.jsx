1b197c5ae2258d1684f5d7b1845b5565b760c369
Manikant Jha <manikantcodes@gmail.com>
2024-01-05 16:39:58 +0530

    character details page image css

--- src/components/characterDetails/CharacterInfoTable.jsx
+++ src/components/characterDetails/CharacterInfoTable.jsx
@@ -3,29 +3,31 @@ import TableRow from "./TableRow";
 import styles from "../../styles/characterInfoTable.module.css";
 
 function CharacterInfoTable(props) {
-  function handleClick() {
-    const table = document.getElementById("detailsPageInfoTable");
-    const button = document.getElementById("toggleTableButton");
-    if (!table.style.display || table.style.display === "table") {
-      button.innerHTML = "Show Info";
-      return (table.style.display = "none");
-    }
-    if (table.style.display === "none") {
-      button.innerHTML = "Hide Info";
-      return (table.style.display = "table");
-    }
-  }
-
-  // function greetings(e, person) {
-  //   console.log(e.target.innerHTML);
-  //   console.log("Ram Ram! " + person);
+  // function handleClick() {
+  //   const table = document.getElementById("detailsPageInfoTable");
+  //   const button = document.getElementById("toggleTableButton");
+  //   if (!table.style.display || table.style.display === "table") {
+  //     button.innerHTML = "Show Info";
+  //     return (table.style.display = "none");
+  //   }
+  //   if (table.style.display === "none") {
+  //     button.innerHTML = "Hide Info";
+  //     return (table.style.display = "table");
+  //   }
   // }
+
+  function greetings(person) {
+    alert("Hello " + person + "!");
+  }
 
   return (
     <div>
       <button
         style={{ width: "100%" }}
-        onClick={handleClick}
+        onClick={function (e) {
+          console.log((e.target.innerHTML = "Button Clicked"));
+          greetings("Shinchan");
+        }}
         id="toggleTableButton"
       >
         Hide Info
