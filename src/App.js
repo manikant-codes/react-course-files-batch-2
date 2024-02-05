ce6c2dea2418901e0a115feb238ed5eaa692b26c
Manikant Jha <manikantcodes@gmail.com>
2024-02-05 15:39:50 +0530

    restructured project structure

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import "./App.css";
-import Home from "./pages/Home";
+import Main from "./pages/Main";
 
 function App() {
-  return (
-    <>
-      <Home />
-    </>
-  );
+  return <Main />;
 }
 
 export default App;
--- src/utils/stringUtils.js
+++
@@ -1,3 +0,0 @@
-export function capitalizeFirstLetter(string) {
-  return string[0].toUpperCase() + string.slice(1).toLowerCase();
-}
