ccc5b3306e78f55c1e6ab52c43d388fa2b78cfff
Manikant Jha <manikantcodes@gmail.com>
2024-02-05 17:11:50 +0530

    useMultipleFetch

--- src/cusatomHooks/useFetch.js
+++ src/cusatomHooks/useFetch.js
@@ -28,3 +28,43 @@ export function useFetch(url) {
 
   return [loading, data, error];
 }
+
+export function useMultipleFetch(url) {
+  const [l, d, e] = useFetch(url);
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(
+    function () {
+      async function fetchData() {
+        try {
+          setLoading(true);
+          let temp = await d.results.map(async (i) => {
+            try {
+              const res = await fetch(i.url);
+              const item = await res.json();
+              return item;
+            } catch (err) {
+              throw err;
+            }
+          });
+          temp = await Promise.all(temp);
+          setData(temp);
+        } catch (error) {
+          console.log(error);
+          setError(error);
+        } finally {
+          setLoading(false);
+        }
+      }
+
+      if (d && d.results && Array.isArray(d.results)) {
+        fetchData();
+      }
+    },
+    [d]
+  );
+
+  return [loading, data, error];
+}
--- src/pages/Pokedex.jsx
+++ src/pages/Pokedex.jsx
@@ -1,17 +1,22 @@
-import React from "react";
-import { useFetch } from "../cusatomHooks/useFetch";
+import React, { useState } from "react";
+import { useFetch, useMultipleFetch } from "../cusatomHooks/useFetch";
+import PokemonCard from "../components/pokedex/PokemonCard";
 
 function Pokedex() {
-  const [loading, data, error] = useFetch(
-    `https://jsonplaceholder.typicode.com/users`
+  // const [loading, data, error] = useFetch(
+  //   `https://pokeapi.co/api/v2/pokemon/?offset=${0}&limit=${20}`
+  // );
+
+  const [loading, data, error] = useMultipleFetch(
+    `https://pokeapi.co/api/v2/pokemon/?offset=${0}&limit=${20}`
   );
 
-  console.log(data);
+  console.log("data", data);
 
   // const [pokemons, setPokemons] = useState(null);
   // const [error, setError] = useState(null);
   // const [loading, setLoading] = useState(false);
-  // const [page, setPage] = useState(0);
+  const [page, setPage] = useState(0);
 
   // async function fetchData() {
   //   const temp = [];
@@ -38,21 +43,21 @@ function Pokedex() {
   //   [page]
   // );
 
-  // function handleNext() {
-  //   setPage(function (p) {
-  //     return ++p;
-  //   });
-  // }
+  function handleNext() {
+    setPage(function (p) {
+      return ++p;
+    });
+  }
 
-  // function handlePrev() {
-  //   setPage(function (p) {
-  //     if (p > 0) {
-  //       return --p;
-  //     } else {
-  //       return 0;
-  //     }
-  //   });
-  // }
+  function handlePrev() {
+    setPage(function (p) {
+      if (p > 0) {
+        return --p;
+      } else {
+        return 0;
+      }
+    });
+  }
 
   if (loading) return <h1>Loading...</h1>;
 
@@ -60,75 +65,75 @@ function Pokedex() {
     return <h1>Something went wrong...</h1>;
 
   return (
-    // <>
-    //   <div
-    //     style={{
-    //       display: "flex",
-    //       justifyContent: "center",
-    //       position: "fixed",
-    //       bottom: 0,
-    //       left: 0,
-    //       right: 0,
-    //       gap: "16px",
-    //       padding: "16px 32px",
-    //       zIndex: 100,
-    //       backgroundColor: "#f5f5f5",
-    //       boxShadow:
-    //         "rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px",
-    //     }}
-    //   >
-    //     <button
-    //       style={{
-    //         backgroundColor: "#f44336",
-    //         color: "white",
-    //         padding: "10px 20px",
-    //         border: "none",
-    //         borderRadius: "5px",
-    //         cursor: "pointer",
-    //         marginLeft: "10px",
-    //       }}
-    //       onClick={handlePrev}
-    //     >
-    //       Perv
-    //     </button>
-    //     <button
-    //       style={{
-    //         backgroundColor: "#4CAF50",
-    //         color: "white",
-    //         padding: "10px 20px",
-    //         border: "none",
-    //         borderRadius: "5px",
-    //         cursor: "pointer",
-    //       }}
-    //       onClick={handleNext}
-    //     >
-    //       Next
-    //     </button>
-    //   </div>
-    //   <div
-    //     style={{
-    //       display: "grid",
-    //       gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
-    //       gridGap: "16px",
-    //       justifyContent: "center",
-    //       padding: "16px",
-    //       width: "100%",
-    //     }}
-    //   >
-    //     {pokemons.map(function (pokemon) {
-    //       return (
-    //         <PokemonCard
-    //           name={pokemon.name}
-    //           image={pokemon.sprites.other["official-artwork"].front_default}
-    //           tags={pokemon.types.map((type) => {
-    //             return type.type.name;
-    //           })}
-    //         />
-    //       );
-    //     })}
-    //   </div>
-    // </>
-    <h1>Pokedex</h1>
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          right: 0,
+          gap: "16px",
+          padding: "16px 32px",
+          zIndex: 100,
+          backgroundColor: "#f5f5f5",
+          boxShadow:
+            "rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px",
+        }}
+      >
+        <button
+          style={{
+            backgroundColor: "#f44336",
+            color: "white",
+            padding: "10px 20px",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+            marginLeft: "10px",
+          }}
+          onClick={handlePrev}
+        >
+          Perv
+        </button>
+        <button
+          style={{
+            backgroundColor: "#4CAF50",
+            color: "white",
+            padding: "10px 20px",
+            border: "none",
+            borderRadius: "5px",
+            cursor: "pointer",
+          }}
+          onClick={handleNext}
+        >
+          Next
+        </button>
+      </div>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+          gridGap: "16px",
+          justifyContent: "center",
+          padding: "16px",
+          width: "100%",
+        }}
+      >
+        {data.map(function (pokemon) {
+          return (
+            <PokemonCard
+              name={pokemon.name}
+              image={pokemon.sprites.other["official-artwork"].front_default}
+              tags={pokemon.types.map((type) => {
+                return type.type.name;
+              })}
+            />
+          );
+        })}
+      </div>
+    </>
+    // <h1>Pokedex</h1>
   );
 }
 
